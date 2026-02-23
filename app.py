5d281aec7b8e353a35cb9a7d720165148043f199
Sofia80562 <mp.beltranm@uea.edu.ec>
2026-02-22 19:42:32 -0500

    Semana 10 actualización
    Finalizando Tarea Semana 10

--- app.py
+++ app.py
@@ -1,27 +1,15 @@
-from flask import Flask
-
+from flask import Flask, render_template 
 app = Flask(__name__)
 
-# Ruta principal con el nombre de tu negocio
 @app.route('/')
-def home():
-    return """
-    <h1>BiblioTech Beltrán - Sistema de Biblioteca</h1>
-    <p>Bienvenido a nuestra plataforma de gestión y consulta de libros.</p>
-    <hr>
-    <p>Prueba las rutas dinámicas para ver cómo funciona el sistema:</p>
-    <ul>
-        <li>Escribe en la URL: <code>/libro/don-quijote</code></li>
-        <li>Escribe en la URL: <code>/libro/el-principito</code></li>
-    </ul>
-    """
+def index():
+    return render_template('index.html') 
 
-# Ruta dinámica para libros
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 @app.route('/libro/<titulo>')
 def consultar_libro(titulo):
-    # Formateamos el título (ej: de 'don-quijote' a 'Don Quijote')
-    nombre_limpio = titulo.replace('-', ' ').title()
-    return f"<h2>Consulta de Libro</h2><p>Título: <strong>{nombre_limpio}</strong> - Estado: <span style='color:green;'>Disponible en Estantería A1</span>.</p>"
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    
+    return render_template('index.html', libro=titulo)
